19090048245105f0954bb91fd869213274bea24c
Dimaaasik <nochevca.dimangio@gmail.com>
2023-04-01 21:29:15 +0300

    Прочитал доку, оказалось что не прописал ретурн в функции, а так она работала нормально - 3 часа на поиск ошибок , а мог просто прочитать документацию) Зато смогу вернуть свое красивое решение через рекурсию))

--- lr2.py
+++ lr2.py
@@ -17,22 +17,39 @@ mid = 1
 fin = 5
 
 def fun_bisect(left, right, fun, eps=1e-6):
-
+    iterations = 0
     if right == fin:
         arr = np.linspace(left, right, 100)
         fig, ax = plt.subplots()
         ax.plot(arr, y(arr))
         ax.axhline(0.0, color='red', linestyle='--')
         plt.show(block=None)
-    x = (left + right) / 2
-
-    if fun(x) == 0 or abs(right - left) <= eps:
-        print(x)
-        return
-    elif (fun(left) > 0 and fun(x) < 0) or (fun(left) < 0 and fun(x) > 0):
-        return fun_bisect(left, x, fun, eps)
-    else:
-        return fun_bisect(x, right, fun, eps)
+    while iterations < 100:
+        iterations = iterations + 1
+        x = (left + right) / 2
+        if(fun(left) > 0 and fun(x) < 0) or (fun(left) < 0 and fun(x) > 0):
+            right = x
+        else:
+            left = x
+    print(x)
+    return x
 
 fun_bisect(min, fin, y)
 fun_bisect(mid, fin, y)
+
+def func(x):
+    return x**2 - 1.0
+
+
+
+fig, ax = plt.subplots()
+ax.plot (x,func(x))
+ax.axhline(0.0,color = 'black', linestyle = '--')
+plt.show(block=None)
+
+try:
+    np.testing.assert_array_almost_equal(fun_bisect(0.0, 3.0, func), optimize.root_scalar(func, bracket=[0.0, 3.0], method="bisect", maxiter=100).root, decimal=7 )
+except AssertionError as E:
+    print('Hi')
+else:
+    print("The implementation is correct.")
