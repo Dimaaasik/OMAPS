dbfd07609e1c0cac2decd43a9c1fe1606aa5ca39
Dimaaasik <nochevca.dimangio@gmail.com>
2023-04-01 18:41:43 +0300

    lr 2 OM

--- lr2.py
+++ lr2.py
@@ -2,58 +2,7 @@ import numpy as np
 from scipy import linalg, optimize
 import matplotlib.pyplot as plt
 
-# x = np.linspace(0.01,5,100)
-#
-# def f(x):
-#     return ( (5 * x) - ( (8 * np.log(x)) ) - 8)
-#
-#
-# fig, ax = plt.subplots()
-# ax.plot (x,f(x))
-# ax.axhline(0.0,color = 'red', linestyle = '--')
-# plt.show(block=None)
-#
-# def bisection(f, a, b, error_tolerance=1.0e-15, max_iterations=50):
-#     iteration = 0
-#     condition = True
-#     while condition:
-#         x = (a + b) / 2
-#         if f(a) * f(x) < error_tolerance:
-#             b = x
-#         else:
-#             a = x
-#         iteration = iteration + 1
-#         condition = iteration < max_iterations
-#     return x
-#
-#
-# print(bisection(f, 0.1, 2))
-#
-#
-# def f(x):
-#     return x**2 - 1.0
-#
-# try:
-#     np.testing.assert_array_almost_equal(bisection(f, 0, 4.0), optimize.root_scalar(f, bracket=[0, 4.0], method="bisect").root, decimal=7)
-# except AssertionError as E:
-#     print(E)
-# else:
-#     print("The implementation is correct.")
-#
-#
-# print('//////////////////////////////////////////////////////////////////////////')
-#
-# x = np.linspace(0,3,100)
-# x[(x>-0.09)&(x<0.08)] = np.nan
-#
-# def ex2(x):
-#     return ((x**3)-(x**2)-1)
-# fig, ax = plt.subplots()
-# ax.plot (x,f(x))
-# ax.axhline(0.0,color = 'red', linestyle = '--')
-# plt.show(block=None)
-#
-# print(bisection(ex2,0.1,2))
+
 
 x = np.linspace(0.01,5,100)
 def y(x):
